7079286ebd8972f3feba28168846fcf03391d3b5
Tijn Kersjes <tijn@divbyzero.nl>
2016-04-19 15:39:32 +0200

    Added transform-runtime plugin - fixes #1

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
         require("babel-preset-es2015"),
         require("babel-preset-stage-1"),
         require("babel-preset-react")
+    ],
+    plugins: [
+        require("babel-plugin-transform-runtime")
     ]
 };
